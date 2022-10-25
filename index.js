f43b7eb56d04b8c06a73b2fd5ea7eebf2f0137ab
theocita <theocitaallen@gmail.com>
2022-10-24 20:58:46 -0400

    Complete Assigment

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  return word == word.split('').reverse().join(''); 
 }
+  
+
 
 /* 
   Add your pseudocode here
+
+ Call isPalindrome with a word as an argument
+  iterate over word forwards and backwords, then rejoin them.
+
+  
 */
 
 /*
   Add written explanation of your solution here
+  I called the function isPalindrome ans passed the argument "word" through it.
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("civic"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("level"));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
